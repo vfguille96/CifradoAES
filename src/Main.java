217d3da2dad512044ab6fd0a9c8849db5384cbc5
Guillermo Vera <vfguille@gmail.com>
2019-02-07 11:49:15 +0100

    Implementando los métodos

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
-import javax.crypto.SecretKey;
+import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 public class Main {
 
     private static String cifrado = "AES";
-    private static SecretKey obtenerClaveOpaca(int longitud){
-        return null;
+    private static SecretKey obtenerClaveOpaca(int longitud) throws NoSuchAlgorithmException {
+        KeyGenerator claveInstancia = KeyGenerator.getInstance(cifrado);
+        claveInstancia.init(longitud); // Por defecto es 128b.
+        return claveInstancia.generateKey(); // Superclave que no se puede desencriptar (hoy en día).
     }
 
     // Se puede acceder a sus partes. Se le llama Spec. Transparente.
@@ -13,15 +19,18 @@ public class Main {
         return null;
     }
 
-    public static String encriptar(String mensaje, SecretKey clave){
-        return null;
+    public static String encriptar(String mensaje, SecretKey clave) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException, BadPaddingException, IllegalBlockSizeException {
+        Cipher cipher = Cipher.getInstance(cifrado);
+        cipher.init(Cipher.ENCRYPT_MODE, clave);
+        byte[] encVal = cipher.doFinal(mensaje.getBytes("utf8"));
+        return Base64.getEncoder().encodeToString(encVal);  // Se necesitan que los bloques sean uniformes y múltiplo de 16.
     }
 
     public static String desencriptar(String criptograma, SecretKey clave){
         return null;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchPaddingException, UnsupportedEncodingException {
         String mensaje = "El método calvá junto con el sort y el wear.";
         String miClave = "Voy a encriptar una mierda con esto.";
 
