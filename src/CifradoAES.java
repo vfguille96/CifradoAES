600f78d71a65d4f2c45fa26c3af3139d097a32b9
Guillermo Vera <vfguille@gmail.com>
2019-02-14 11:52:40 +0100

    Implementando clave transparente

--- src/CifradoAES.java
+++ src/CifradoAES.java
@@ -2,10 +2,11 @@ import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
-public class Main {
+public class CifradoAES {
 
     private static String cifrado = "AES";
     private static SecretKey obtenerClaveOpaca(int longitud) throws NoSuchAlgorithmException {
@@ -15,7 +16,9 @@ public class Main {
     }
 
     // Se puede acceder a sus partes. Se le llama Spec. Transparente.
-    public static SecretKeySpec obtenrClaveTransparente(String miClave){
+    public static SecretKeySpec obtenrClaveTransparente(String miClave) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        byte[] miClaveEnBytes =  messageDigest.digest(miClave.getBytes("utf8")); // Múltiplo de 16
         return null;
     }
 
@@ -26,8 +29,12 @@ public class Main {
         return Base64.getEncoder().encodeToString(encVal);  // Se necesitan que los bloques sean uniformes y múltiplo de 16.
     }
 
-    public static String desencriptar(String criptograma, SecretKey clave){
-        return null;
+    public static String desencriptar(String criptograma, SecretKey clave) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        Cipher c = Cipher.getInstance(cifrado);
+        c.init(Cipher.DECRYPT_MODE, clave);
+        byte[] decValue = Base64.getDecoder().decode(criptograma);
+        byte[] decryptedValue = c.doFinal(decValue);
+        return new String(decryptedValue);
     }
 
     public static void main(String[] args) throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchPaddingException, UnsupportedEncodingException {
